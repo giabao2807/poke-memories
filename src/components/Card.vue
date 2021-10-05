24c6f0192e69674bdacf6f449ca96951ff8a023b
GiaBaone <102190252@sv1.dut.udn.vn>
2021-10-05 12:28:22 +0700

    update style for card 5.10.21

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="card">
-    <div class="card__inner">
+    <div class="card__inner" :class="{ 'is-flipped' : isFlipped }">
       <div class="card__face card__face--font">
         <div class="card_content">Font</div>
       </div>
@@ -11,11 +11,28 @@
   </div>
 </template>
 
+<script>
+export default {
+  data() {
+    return {
+      isFlipped:false,
+    };
+  },
+  methods: {
+    onToggeleFlipCard(){
+      this.isFlipped = !this.isFlipped;
+      },
+  },
+}
+</script>
+
 <style lang="css" scoped>
   .card{
     display:inline-block;
     margin-right:1rem;
     margin-bottom:1rem;
+    width: 90px;
+    height: 120px;
   }
 
   .card__inner{
@@ -27,5 +44,21 @@
   }
 
   .card__inner.is-flipped{
+    transform: rotateY(-180deg);
   }
+  
+  .card__face{
+    position:absolute;
+    width: 100%;
+    height: 100%;
+    backface-visibility: hidden;
+    overflow: hidden;
+    border-radius: 1rem;
+    padding: 1rem;
+    box-shadow: 0px 3px 10px 3px rgba(0,0,0,0.2);
+    }
+  .card__face--back{
+      background-color: var(--light);
+      transform: rotateY(-180deg) ;
+    }
 </style>
